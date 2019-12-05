1542fc97096643f704b503a4cbb0871035ff34c1
MrJuniorice <43075764+MrJuniorice@users.noreply.github.com>
2019-12-04 20:30:40 -0500

    test 2

---
+++ lab-vr/Lab 1 VR/Assets/Collect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Collect : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    private int count;
+    private object col;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 2)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Pick Up")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
